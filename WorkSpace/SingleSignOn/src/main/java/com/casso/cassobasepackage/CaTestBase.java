283c0796d17af351f64b7b1b7dcdbd3911392e1d
Nagarjun Surampally <nagarjun.surampally@broadcom.com>
2019-11-18 14:18:31 +0530

    test

--- WorkSpace/SingleSignOn/src/main/java/com/casso/cassobasepackage/CaTestBase.java
+++ WorkSpace/SingleSignOn/src/main/java/com/casso/cassobasepackage/CaTestBase.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Test;
 
 public class CaTestBase {
 	public WebDriver driver;
