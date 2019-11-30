9f2bb09824ffcb006d4490e7957dec069a81b2db
koteswarao tekkem <koteswarao.tekkem@gmail.com>
2019-11-30 23:47:20 +0530

    refacotring

--- SikRest/src/test/java/com/freecrm/testcases/CreateUserTest.java
+++ SikRest/src/test/java/com/freecrm/testcases/CreateUserTest.java
@@ -1,62 +1,58 @@
-package client.core;
-
-import java.util.List;
+package com.freecrm.testcases;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.freecrm.base.CRMTestBase;
-import com.freecrm.config.CommonUtils;
 import com.freecrm.pages.HomePage;
 import com.freecrm.pages.LoginPage1;
 import com.freecrm.pages.ReportsPage;
 
-public class UserCreattionTest extends CRMTestBase {
-
+public class CreateUserTest extends CRMTestBase {
 	@BeforeClass
-	public void beforeClassInTest() {
-		System.out.println("CopyOfCreateUserTest");
+	public void beforeClassInTest(){
+		System.out.println("beforeClassInTest");
+	}
+	
+	@BeforeClass
+	public void runbeforeMethod(){
+		System.out.println("runbeforeMethod");
 		initDriver();
 	}
-
-	@BeforeMethod
-	public void runbeforeMethod() {
-		System.out.println(" CopyOfCreateUserTest runbeforeMethod");
-
-	}
-
-	@Test(priority = 0, description = "creating Super User", groups = "smoke")
-	public void createSuperUser() {
+	
+	@Test(priority=0, description="creating Super User", groups="smoke")
+	public void createSuperUser(){
 		LoginPage1 loginObj = new LoginPage1();
-		HomePage homepage = loginObj.loginAsSuperUser();
+		HomePage homepage =loginObj.loginAsSuperUser();
 		System.out.println(homepage.getHomePageTitle());
 		driver.switchTo().frame("mainpanel");
-
+		
 		homepage.verifyHomePageDetails();
 		ReportsPage reportsPAge = homepage.openReports();
 		reportsPAge.verifyCallReports();
-
-		driver.findElement(By.xpath("//*[@id='navmenu']/ul/li[15]/a")).click();
+		
+	driver.findElement(By.xpath("//*[@id='navmenu']/ul/li[15]/a")).click();
 		homepage.openCalanderPage();
 		System.out.println("create super user ");
 		Assert.assertEquals(33, 33, "actaul nd expced are not same");
 		driver.switchTo().defaultContent();
 	}
-
-	@Test(priority = 1, description = "createSupAdmin  erU  ser", enabled = true, dependsOnMethods = "createSuperUser")
-	public void createAdminUser() {
+	
+	@Test(priority=1, description="createSupAdmin  erU  ser", enabled= true, dependsOnMethods="createSuperUser")
+	public void createAdminUser(){
 		System.out.println("createAdminUsercreateAdminUser");
 	}
-
+	
 	@AfterClass
-	public void closeBrowser() {
+	public void closeBrowser(){
 		System.out.println("closin gthe browser");
-		driver.close();
+		driver.quit();
 	}
-
+	
+	
 }
+
+
